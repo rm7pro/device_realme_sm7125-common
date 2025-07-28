89a79b68def33b7c5b2857e73a53cbf190f777d2
shaenix <theshaenix@gmail.com>
2025-07-28 18:40:19 +0530

    sm7125-common:Fixed the typo in vndk_v30_lib

--- common.mk
+++ common.mk
@@ -448,7 +448,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libgui.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libxxx.so \
-    prebuilts/vndk/v30/arm64/arch-arm-armv8-a/shared/vndk-core/libgui.so:$(TARGET_COPY_OUT_SYSTEM)/lib6/libxxx.so \
+    prebuilts/vndk/v30/arm64/arch-arm-armv8-a/shared/vndk-core/libgui.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libxxx.so \
 
 # Vulkan
 PRODUCT_COPY_FILES += \
