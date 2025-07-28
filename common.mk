0b2704532bb6c7542bcf5be9e3965f5de22f588e
shaenix <theshaenix@gmail.com>
2025-07-28 18:02:22 +0530

    sm7125-common: Add vendor service manager

--- common.mk
+++ common.mk
@@ -457,6 +457,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
