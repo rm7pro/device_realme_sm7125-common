7343655e1fe3f88fe7caf1340750050e2cabf562
shaenix <theshaenix@gmail.com>
2025-07-28 20:37:15 +0530

    sm7125-common: Add feature flag for IPSEC_TUNNEL_MIGRATION This commit adds the IPSEC_TUNNEL_MIGRATION feature flag, with XFRM_MIGRATE used as the implementation

--- common.mk
+++ common.mk
@@ -323,7 +323,8 @@ PRODUCT_PACKAGES += \
 
 # Network
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml 
 
 # NFC
 PRODUCT_PACKAGES += \
