162b77bd85713f853c10ad39ba8ba15fa9c52a47
shaenix <theshaenix@gmail.com>
2025-07-28 19:49:58 +0530

    sm7125-common: Explicitly disable virtual A/B

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,6 +194,9 @@ TARGET_COPY_OUT_ODM := odm
 
 BOARD_USES_METADATA_PARTITION := true
 
+# Explicitly disable virtual A/B
+BOARD_VIRTUAL_AB_ENABLE := false
+
 BOARD_FLASH_BLOCK_SIZE := 262144
 
 # Platform
