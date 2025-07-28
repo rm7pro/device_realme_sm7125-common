81ac965ff2e8130bf71f792e6bd5d41ee3d591c7
shaenix <theshaenix@gmail.com>
2025-07-28 19:12:56 +0530

    sm7125-common:Switch to QTI Memtrack AIDL HAL

--- common.mk
+++ common.mk
@@ -116,7 +116,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libtinyxml \
     libgui_vendor \
-    memtrack.atoll \
     libsdmcore \
     libsdmutils \
     libvulkan
@@ -126,8 +125,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    vendor.qti.hardware.memtrack-service \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
