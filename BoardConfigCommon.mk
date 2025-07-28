a292f231af227a18a14f5ca35b2a31fa6f27d999
shaenix <theshaenix@gmail.com>
2025-07-28 17:11:33 +0530

    sm7125-common: Include the lineage framework_matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,8 +93,12 @@ BOARD_HAS_QCA_FM_SOC := cherokee
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml
+
+DEVICE_MANIFEST_FILE := \
+    $(COMMON_PATH)/manifest.xml
 
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
