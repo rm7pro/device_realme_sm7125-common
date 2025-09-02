8777433bfa93b9dbb7f3fb99275ba615ca5d3260
theshaenix <theshaenix@gmail.com>
2025-09-02 22:21:56 +0530

    sm7125-common: Switch to AIDL LiveDisplay HAL

--- common.mk
+++ common.mk
@@ -271,7 +271,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.realme_sm7125
 
-# LiveDisplay
+#Lawnchair
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+
+#LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+
+# Media
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
