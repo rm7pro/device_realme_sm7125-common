cf2aa30e7286cc26a2786f5f34d9dee7dc61417d
theshaenix <theshaenix@gmail.com>
2025-11-09 15:15:07 +0530

    sm7125-common: Migrate to lineage power adil

--- common.mk
+++ common.mk
@@ -348,7 +348,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr 
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -407,7 +407,7 @@ PRODUCT_SOONG_NAMESPACES += \
     kernel/realme/sm7125 \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
+    hardware/lineage/interfaces/power-libperfmgr 
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
